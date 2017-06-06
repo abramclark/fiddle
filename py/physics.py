79f449501a9cc40519aa776bd57127ac43bfb14b
Abram Clark <abram@easypost.com>
2017-06-06 15:29:04 -0700

    Added mi2km and mph2kph

--- py/physics.py
+++ py/physics.py
@@ -1,4 +1,4 @@
-import math
+from math import *
 import periodictable as pt # todo: add melting and boiling temps
 
 G = 6.67408e-11 # * m**3 / (kg * s) # gravitational constant
@@ -13,5 +13,7 @@ mevtokg = 9.0958e-31 # conversion of MeV to Kg
 # regular units
 lb2kg = 0.453592 # kg/pound
 inch2cm = 2.54 # cm/inch
+mi2km = 1.609344
+kph2mph = 0.621371 # kph/mph
 
 atm = 101325 # atmospheric pressure kg*m^2/s^2 (pascals)
