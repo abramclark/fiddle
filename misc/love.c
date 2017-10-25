e59eb9f8018b1a156491822c802641ea24481ed9
Abram Clark <abram@spez.local>
2017-10-25 15:53:10 -0700

    Moved misc junk to misc folder

---
+++ misc/love.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("%d",printf("%d   \b\b\b",printf(" %d",printf("%s","ILOVECPROGRAM"))));
+    //printf("%d", printf("foo"));
+    //printf("%d", printf("barz"));
+}
