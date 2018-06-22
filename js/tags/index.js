ace2779c92b8e7e00d41afcbb4d745f8297d1782
Abram Clark <abram@spez.local>
2018-06-22 00:32:25 -0700

    Started prompt UI

--- js/tags/index.js
+++ js/tags/index.js
@@ -22,7 +22,7 @@ var Tags = ()=>{ var o = {
 
     get_tags: id => o.items[id].tags,
 
-    sort_by_count: ()=> o.tags.sort((a,b)=> b.items.length - a.items.length),
+    sort_by_count: ()=> o.tags.sort((a, b)=> b.items.length - a.items.length),
 
     items_intersecting: tags =>{
         if( !tags.length ) return []
@@ -48,32 +48,35 @@ var Tags = ()=>{ var o = {
 
     tags_from_items: items =>{
         if( !items.length ) return []
-        return freq_sort(items.map(x => x.tags).reduce((a,b)=> a.concat(b)))
+        return freq_sort(items.map(x => x.tags).reduce((a, b)=> a.concat(b)))
     },
 }; return o }
 
 
 var TagsView = (tags_el, items_el)=>{ var o = {
     db: Tags(),
-    tags_el: $(tags_el), items_el: $(items_el),
-    with_tags: new Set(), without_tags: new Set(),
-    active_tag: false, tags_visible: new Set(),
-    selected_items: new Set(), active_item: false,
-    refresh_items_timer: false,
+    tags_el:$(tags_el), items_el:$(items_el),
+    with_tags:new Set(), without_tags:new Set(),
+    active_tag:false, tags_visible:new Set(),
+    selected_items:[], active_item:false,
+    refresh_items_timer:false,
 
-    min_tag_size: .8, max_tag_size: 1.5,
+    min_tag_size:.8, max_tag_size:1.5,
 
     item_els: ()=> o.items_el.children(),
     tag_els: ()=> o.tags_el.children(),
 
-    item_el: x => document.getElementById('i' + x.id),
-    tag_el: x => document.getElementById('t' + x.id),
+    item_el:x => document.getElementById('i' + x.id),
+    tag_el :x => document.getElementById('t' + x.id),
 
     render_tag: (tag, max_count)=>{
-        var el = $('<div>').addClass('tag').html(tag.name).css('font-size',
-            interp(o.min_tag_size, o.max_tag_size, tag.items.length / max_count)
-                + 'em'
-        ).on('click', ev =>{
+        var el = $('<div>').addClass('tag').html(tag.name)
+        el[0].id = 't' + tag.id
+
+        el.css('font-size', interp(o.min_tag_size, o.max_tag_size,
+            tag.items.length / max_count) + 'em')
+
+        el.on('click', ev =>{
             var pick_set = 'with_tags', other_set = 'without_tags'
             if( ev.shiftKey )
                 [pick_set, other_set] = ['without_tags', 'with_tags']
@@ -98,8 +101,29 @@ var TagsView = (tags_el, items_el)=>{ var o = {
             o.refresh()
         }).on('mousedown', ()=> false)
 
-        el[0].id = 't' + tag.id
         return el
+    },
+
+    render_item: item =>{
+        var el = $('<div class=item>')
+        el[0].id = 'i' + o.db.add_item(item)
+
+        $('<img>').attr('src', item.icon).appendTo(el)
+        $('<a>').attr('href', item.url).text(item.name).appendTo(el)
+
+        $('<div class=selector>').appendTo(el).on('click', ev =>{
+            $(o.item_el(item)).toggleClass('selected',
+                toggle_list_element(o.selected_items, item))
+        })
+
+        return el.on('mouseover', ev =>{
+            o.active_item = item
+            o.refresh()
+        }).on('mouseout', ev =>{
+            if( !o.active_item ) return
+            o.active_item = false
+            o.refresh()
+        })
     },
 
     refresh: ()=>{
@@ -129,36 +153,22 @@ var TagsView = (tags_el, items_el)=>{ var o = {
         }, 50)
     },
 
-    render_item: item =>{
-        var el = $('<div class=item>')
-        el[0].id = 'i' + o.db.add_item(item)
-        $('<img>').attr('src', item.icon).appendTo(el)
-        $('<a>').attr('href', item.url).text(item.name).appendTo(el)
-
-        $('<div class=selector>').appendTo(el).on('click', ev =>{
-            $(o.item_el(item)).toggleClass('selected',
-                toggle_element(o.selected_items, item))
-        })
-
-        return el.on('mouseover', ev =>{
-            o.active_item = item
-            o.refresh()
-        }).on('mouseout', ev =>{
-            if( !o.active_item ) return
-            o.active_item = false
-            o.refresh()
-        })
-    },
-
+    cmd_el:$('#cmd')[0],
     handle_keys: ev =>{
-        console.log(ev.which)
+        var cc = ev.which, c = String.fromCharCode(cc)
+        if((cc >= 32) && (cc <= 126) || (cc == 8) || (cc == 46))
+            o.cmd_el.focus()
     },
 
     add_items: items =>{
-        o.items_el.append(items.map(o.render_item))
-        o.db.sort_by_count()
-        o.tags_el.append(o.db.tags.map(t =>
-            o.render_tag(t, o.db.tags[0].items.length)))
+        setTimeout(()=>{
+            o.items_el.append(items.map(o.render_item))
+            o.db.sort_by_count()
+        }, 1)
+        setTimeout(()=>{
+            o.tags_el.append(o.db.tags.map(t =>
+                o.render_tag(t, o.db.tags[0].items.length)))
+        }, 1)
         window.onkeydown = o.handle_keys
     }
 }; return o }
@@ -169,19 +179,21 @@ var set_default = (o, key, val)=> o.hasOwnProperty(key) ? o[key] : o[key] = val
 var interp = (start, end, p)=> (end - start) * p + start
 
 var freq_sort = xs =>{
-    var cm = xs.reduce((map, x)=>{
+    var counts = xs.reduce((map, x)=>{
         map.has(x) ? map.set(x, map.get(x) + 1) : map.set(x, 1)
         return map
     }, new Map())
-    return [...cm.keys()].sort((a,b)=> cm.get(b) - cm.get(a))
+    return [...counts.keys()].sort((a, b)=> counts.get(b) - counts.get(a))
 }
 
-var toggle_element = (s, x)=>{
-    if( !s.delete(x) ){
-        s.add(x)
-        return true
+var toggle_element = (s, x)=>{ if( !s.delete(x) ) return s.add(x) }
+var toggle_list_element = (l, x)=>{
+    var ix = l.indexOf(x)
+    if(ix == -1){
+        l.push(x)
+        return l
     }
-    return false
+    l.splice(ix, 1)
 }
 
 
