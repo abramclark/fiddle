164a8410ca3166232af07e7a0036104193835d1d
Abram Clark <abram@easypost.com>
2021-07-12 21:07:32 -0700

    Added velocity field web sketch in web/draw/field. Little physics.py updates. Little sierpinski draw function

--- py/physics.py
+++ py/physics.py
@@ -1,5 +1,8 @@
 from math import *
-import periodictable as pt # todo: add melting and boiling temps
+try:
+    import periodictable as pt # todo: add melting and boiling temps
+except ModuleNotFoundError as e:
+    print(e)
 
 Av = 6.022140857e23 # # Avogadro constant
 G = 6.67408e-11 # m**3 / (kg * s) # gravitational constant
@@ -31,3 +34,9 @@ atm = 101325 # P # 1 atmosphere # pressure at altitude 0
 # K = Kelvin
 # acceleration = m / s**2
 # m = meters
+
+
+class Solar:
+    class Earth:
+        mass = 5.97237e+24
+        radius = 6371
