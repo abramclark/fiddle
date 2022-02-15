ed99f8f3ce710971470b4e72332a43c9c7b38e79
Abram Clark <abram@easypost.com>
2022-02-15 00:01:57 -0800

    Index of word in name to unicode chars

---
+++ py/unicode_index.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+import unicodedata
+
+
+def get_name(cp):
+    try:
+        return unicodedata.name(chr(cp))
+    except:
+        return None
+
+
+def unicode_index():
+    code_names = ((get_name(i), i) for i in range(0, 2**20))
+    code_words = ((n.lower().split(), i) for n, i in code_names if n)
+
+    index = defaultdict(str)
+    for words, i in code_words:
+        for w in words:
+            index[w] += chr(i)
+    return index
